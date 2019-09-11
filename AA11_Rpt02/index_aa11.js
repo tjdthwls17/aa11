470d67d3766be5defe623eb72e8fd98b00769898
tjdthwls17 <54882152+tjdthwls17@users.noreply.github.com>
2019-09-11 16:49:53 +0900

    Add files via upload

---
+++ AA11_Rpt02/index_aa11.js
@@ -0,0 +1,9 @@
+// index_aa13.js
+
+var myinfo = require('./aanninfo');
+
+myinfo("aa13", "Shinsongju", '010-1234-5678');
+
+myinfo("aa12", "Kimhaein", '010-1234-5678');
+
+myinfo("aa11", "Seongsojin", '010-1234-5678');
