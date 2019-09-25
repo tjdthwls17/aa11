09dc986155c04ae29d940259ea261240afc01fc9
tjdthwls17 <54882152+tjdthwls17@users.noreply.github.com>
2019-09-25 16:34:17 +0900

    Add files via upload

---
+++ AA11_rpt04/arduino/sketch05_multi_signals/sketch05_multi_signals.ino
@@ -0,0 +1,35 @@
+int humi = 0;
+int temp = 0;
+int lux = 0;
+
+int ledR = 3;
+int ledG = 5;
+int ledB = 6;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  humi = random(40,90);
+  temp = random(-10,30);
+  lux = random(150,250);
+  pwmLed(ledR , humi);
+  pwmLed(ledG , temp);
+  pwmLed(ledB , lux);
+
+  Serial.print("AA11, Ambient lux: ");
+  Serial.print(lux);
+  Serial.print(" , Humidity: ");
+  Serial.print(humi);
+  Serial.print(" , Temperature: ");
+  Serial.println(temp);
+  delay(500);
+
+}
+void pwmLed(int led, int pwmValue) {
+  analogWrite(led, pwmValue);
+  delay(10);
+}
---
+++ AA11_rpt04/arduino/sketch06_analog_f_map/sketch06_analog_f_map.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+int sensorValue = analogRead(A0);
+
+float voltage = f_map(sensorValue, 0, 1023, 0.0, 5.0);
+
+Serial.print("AA11, Present voltage (0.0 ~ 5.0) : ");
+Serial.println(voltage);
+delay(500);
+}
+float f_map(long x, long in_min, long in_max, float out_min, float out_max)
+{
+  return(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
