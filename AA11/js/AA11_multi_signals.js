d170928d5e941ea16afd84fbc5546e5942307f12
tjdthwls17 <54882152+tjdthwls17@users.noreply.github.com>
2019-10-16 20:34:08 +0900

    Add files via upload

---
+++ AA11/js/AA11_multi_signals.js
@@ -0,0 +1,174 @@
+
+// 'use strict';
+
+// tmp36_node.js
+
+ 
+
+var serialport = require('serialport');
+
+var portName = 'COM3';  // check your COM port!!
+
+var port    =   process.env.PORT || 3000;
+
+ 
+
+var io = require('socket.io').listen(port);
+
+ 
+
+// serial port object
+
+var sp = new serialport(portName,{
+
+    baudRate: 9600,   // 9600  38400
+
+    dataBits: 8,
+
+    parity: 'none',
+
+    stopBits: 1,
+
+    flowControl: false,
+
+    parser: serialport.parsers.readline('\r\n')  // new serialport.parsers.Readline
+
+});
+
+ 
+
+var dStr='';
+
+var mdata = [];  // temperature
+
+var readData='';
+
+var lux='';
+
+var humi='';
+
+var temp='';
+
+var firstcommaidx=0;
+
+var secondcommaidx=0;
+
+ 
+
+sp.on('data', function (data) { // call back when data is received
+
+    // raw data only 
+
+        //console.log(data); 
+
+        // dStr=getDateString();       
+
+        // tdata[0]=dStr;
+
+        // tdata[1] = data;  // data
+
+        // console.log('AA11, ' + tdata);
+
+        // io.sockets.emit('message', tdata);  // send data to all clients  
+
+ 
+
+        readData=data.toString();
+
+        firstcommaidx=readData.indexOf(',');
+
+        secondcommaidx=readData.indexOf(',',firstcommaidx+1);
+
+ 
+
+        if(firstcommaidx>0){
+
+        lux=readData.substring(0,firstcommaidx);
+
+        humi=readData.substring(firstcommaidx + 1,secondcommaidx);
+
+        temp=readData.substring(secondcommaidx+1);
+
+        readData='';
+
+ 
+
+        dStr=getDateString();
+
+        mdata[0]=dStr;
+
+        mdata[1]=lux;
+
+        mdata[2]=humi;
+
+        mdata[3]=temp;
+
+        
+
+        console.log("AA11, "+mdata);
+
+        io.sockets.emit('message, '+mdata);
+
+        // dStr=getDateString();       
+
+        // tdata[0]=dStr;
+
+        // tdata[1] = data;  // data
+
+        // console.log('AA10, ' + tdata);
+
+        // io.sockets.emit('message', tdata);  // send data to all clients   
+
+    } else {
+
+        console.log(readData);
+
+    } 
+
+});
+
+ 
+
+ 
+
+io.sockets.on('connection', function (socket) {
+
+    // If socket.io receives message from the client browser then 
+
+    // this call back will be executed.
+
+    socket.on('message', function (msg) {
+
+        console.log(msg);
+
+    });
+
+    // If a web browser disconnects from Socket.IO then this callback is called.
+
+    socket.on('disconnect', function () {
+
+        console.log('disconnected');
+
+    });
+
+});
+
+ 
+
+// helper function to get a nicely formatted date string for IOT
+
+function getDateString() {
+
+    var time = new Date().getTime();
+
+    // 32400000 is (GMT+9 Korea, GimHae)
+
+    // for your timezone just multiply +/-GMT by 3600000
+
+    var datestr = new Date(time +32400000).
+
+    toISOString().replace(/T/, ' ').replace(/Z/, '');
+
+    return datestr;
+
+}
